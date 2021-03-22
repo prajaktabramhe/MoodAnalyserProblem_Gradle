da60b02f7c27edabf18ff41e86a3d4bcc385fa16
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-23 01:12:06 +0530

    [Prajakta]:  called analysemood function with message as parameter and return Happy or Sad Mood

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -1,4 +1,22 @@
 package com.bridgelabz.moodanalyser;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MoodAnalyserTest {
+    @Test
+    public  void givenMessage_WhenSad_ShouldReturnSad()
+    {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Sad Message");
+        Assertions.assertEquals("SAD", mood);
+    }
+    @Test
+    public  void givenMessage_WhenNotSad_ShouldReturnHappy()
+    {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Happy Message");
+        Assertions.assertEquals("Happy", mood);
+
+    }
 }
